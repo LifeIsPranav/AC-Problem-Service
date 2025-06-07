254a01abab099fed7ba522ead46c8da051b61b9d
Pranav <lifeispranav@gmail.com>
2025-06-07 21:27:34 +0530

    Added middleware for json, text and urlEncoding Format

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+
+
 const { PORT } = require('./config/server.config')
-
-
 
 const app = express()
 
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/ping', (req, res) => {
+  return res.send({
+    message: "Problem Service is Alive!"
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server Started at http://localhost:${PORT}/`);

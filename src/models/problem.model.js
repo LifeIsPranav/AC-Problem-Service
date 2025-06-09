0e99c8cb97078490dd916de2dc94e452c8e6c0c6
Pranav <lifeispranav@gmail.com>
2025-06-09 18:30:22 +0530

    Schema of Problms created

---
+++ src/models/problem.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+
+const problemSchema = new mongoose.Schema({
+
+  title: {
+    type: String,
+    required: [true, "Title of a Problem cannot be Empty"]
+  },
+
+  description: {
+    type: String,
+    required: [true, "Description of a Problem cannot be Empty"]
+  },
+
+  difficulty: {
+    type: String,
+    enum: ['Easy', 'Medium', 'Hard'],
+    default: 'Easy'
+  },
+
+  testCases: {
+    type: [{
+      input: {
+        type: String,
+        required: true
+      },
+      output: {
+        type: String,
+        required: true
+      },
+    }],
+    required: true
+  },
+
+  editorial: {
+    type: String
+  },
+
+})
+
+const problemModel = mongoose.Model("problem", problemSchema)

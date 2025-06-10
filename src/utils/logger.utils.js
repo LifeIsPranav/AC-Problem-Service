592b0a8004f954ed1f7a29b1ca664a5bb30782a7
Pranav <lifeispranav@gmail.com>
2025-06-10 12:23:13 +0530

    Feat: Added a utility logEvent

---
+++ src/utils/logger.utils.js
@@ -0,0 +1,25 @@
+const logger = require("../config/logger.config");
+
+/**
+ * Utility function to create consistent log messages
+ * @param {string} level - Log level (info, warn, error, debug)
+ * @param {string} message - The log message
+ * @param {string} operation - The operation being performed
+ * @param {string} context - Context (service/repository)
+ * @param {object} [additionalData] - Additional metadata
+ */
+function logEvent(level, message, operation, context, additionalData = {}) {
+    // Create metadata object with only additional data
+    const metadata = {
+        ...additionalData
+    };
+    
+    // Set operation and context as properties on the logger
+    logger.operation = operation;
+    logger.context = context;
+    
+    // Call logger with proper format
+    logger[level](message, metadata);
+}
+
+module.exports = logEvent

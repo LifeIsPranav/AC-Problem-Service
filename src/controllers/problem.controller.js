f0ec2c34e3c543b51c68af8fe98b2bd9e2d2a5ac
Pranav <lifeispranav@gmail.com>
2025-06-10 17:02:02 +0530

    Feat: added Logs to all the controllers

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -5,6 +5,9 @@ const { ProblemService } = require('../services')
 const { StatusCodes } = require('http-status-codes')
 const { ProblemRepository } = require('../repositories')
 const BadRequest = require("../errors/clientSide/badRequest.error")
+const { logInfo, logError, logWarn } = require("../utils/logger.utils")
+const { controller: context } = require("../config/context.config")
+
 
 
 const problemService = new ProblemService(new ProblemRepository())
@@ -15,27 +18,41 @@ function pingProblemController(req, res) {
 
 
 async function addProblem(req, res) {
+  const operation = 'addProblem'
   try {
     const problemData = req.body
-    const problem = await problemService.createProblem(problemData)
 
+    logInfo('Delegated Task to Create Problem Service', operation, context)
+    const problem = await problemService.createProblem(problemData)
+    
+    logInfo('Received Response from the Service', operation, context)
     responseHandler(req, res, StatusCodes.CREATED, "New Problem Created Successfully", problem)
 
   } catch (error) {
+    logError(error, 'Error Fetching Data from the Service', operation, context)
     throw error
   }
 }
 
 
 async function getProblem(req, res) {
+  const operation = 'getProblem'
   try {
     const problemId = req.params.id
-    const problem = await problemService.getProblem(problemId)
 
-    if(!problemId) throw new NotFound()
+    logInfo('Delegated Task to Get Problem Service', operation, context)
+    const problem = await problemService.getProblem(problemId)
+    
+    if(!problem) {
+      logWarn('No Such Problem Found', operation, context, {problemId})
+      throw new NotFound()
+    }
+
+    logInfo('Problem Fetched Successfully from the Service', operation, context)
     responseHandler(req, res, StatusCodes.OK, "Problem Fetched Successfully", problem)
 
   } catch (error) {
+    logError(error, 'Error Fetching Data from the Service', operation, context)
     throw error
   }
 
@@ -43,17 +60,22 @@ async function getProblem(req, res) {
 
 
 async function getProblems(req, res) {
+  operation = 'getProblems'
   try {
+    logInfo('Delegated Task to Get Problems Service', operation, context)
     const problems = await problemService.getAllProblems()
+
 
     const data = {
       totalProblems : problems.length,
       problems
     }
 
+    logInfo('Received Response from the Get Problems Service', operation, context)
     responseHandler(req, res, 200, "All problems Fetched Successfully", data)
     
   } catch (error) {
+    logError(error, 'Error Fetching Data from the Service', operation, context)
     throw error
   }
 }
@@ -63,14 +85,19 @@ async function deleteProblem(req, res) {
   
   try {
     const problemId = req.params.id
-    if(!problemId) throw new BadRequest("Problem Id", "Please Provide Adequate Problem Id")
+    if(!problemId) {
+      logWarn('No ProblemId Provided', operation, context)
+      throw new BadRequest("Problem Id", "Please Provide Adequate Problem Id")
+    }
 
+    logInfo('Delegated Task to Delete Problems Service', operation, context)
     const problem = await problemService.deleteProblem(problemId)
-    if(!problem) throw new NotFound()
 
+    logInfo('Received Response from the Delete Problems Service', operation, context)
     responseHandler(req, res, StatusCodes.OK, "Problem Deleted Successfully", problem)
 
   } catch (error) {
+    logError(error, 'Error Fetching Data from the Service', operation, context)
     throw error
   }
 }
@@ -82,13 +109,23 @@ async function updateProblem(req, res) {
     const problemId = req.params.id
     const details = req.body
 
-    if(!details) throw new BadRequest("Details", "Kindly Provide Details to Be Updated")
-    if(!problemId) throw new BadRequest("Problem Id", "Please Provide Adequate Problem Id")
+    if(!details){
+      logWarn('No Updation Details Provided', operation, context)
+      throw new BadRequest("Details", "Kindly Provide Details to Be Updated")
+    }
+    if(!problemId){ 
+      logWarn('No ProblemId Provided', operation, context)
+      throw new BadRequest("Problem Id", "Please Provide Adequate Problem Id")
+    }
 
+    logInfo('Delegated Task to Update Problems Service', operation, context)
     const updatedProblem = await problemService.updateProblem(problemId, details)
+
+    logInfo('Received Response from the Update Problems Service', operation, context)
     responseHandler(req, res, StatusCodes.OK, "Problem Updated Successfully", updateProblem)
     
   } catch (error) {
+    logError(error, 'Error Fetching Data from the Service', operation, context)
     throw error
   }
 }
